8001d6f23bac4b27a0e409edc75be02fc48fd6c9
Jinnisy <qli036@e.ntu.edu.sg>
2024-12-11 03:41:38 +0000

    1

--- app.py
+++ app.py
@@ -9,5 +9,15 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/SA",methods=["GET","POST"])
+def SA():
+    return(render_template("SA.html"))
+
+@app.route("/SA_result",methods=["GET","POST"])
+def SA_result():
+    q=request.form.get("q")
+    r=textblob.TextBlob(q).sentiment
+    return(render_template("SA_result.html",r=r))
+
 if __name__=="__main__":
     app.run()
